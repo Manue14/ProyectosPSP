bf22047e36375302e7fa2da27cacde601489490f
Manue14 <mnalvarezgomez@gmail.com>
2025-02-20 00:44:02 +0100

    Mejorada base de datos del ejercicio de preguntas y respuestas

--- Ejercicio Preguntas_Respuestas/PreguntasBD.sql
+++ Ejercicio Preguntas_Respuestas/PreguntasBD.sql
@@ -49,65 +49,118 @@ CREATE TRIGGER block_respuestas_insert BEFORE INSERT ON respuestas
 			END IF;
 		END $$
         
-CREATE TRIGGER after_delete_pregunta BEFORE DELETE ON preguntas
+CREATE TRIGGER delete_orphan_preguntas AFTER DELETE ON preguntas
 	FOR EACH ROW
 		BEGIN
-			SET @using_delete_trigger = TRUE;
-            SET @count = 0;
-            SELECT COUNT(*) FROM preguntas_respuestas WHERE id_pregunta = OLD.id INTO count;
-            IF @count = 1
-				DELETE respuestas FROM respuestas WHERE 
-            END IF;
-			DELETE respuestas FROM respuestas
-				LEFT JOIN preguntas_respuestas ON preguntas_respuestas.id_respuesta = respuestas.id
-					WHERE preguntas_respuestas.id_pregunta IS NULL;
-			SET @using_delete_trigger = FALSE;
+			IF NOT @using_delete_trigger THEN	/*Para evitar que el trigger se dispare si es llamada por el otro trigger de borrado*/
+        
+				SET @using_delete_trigger = TRUE;
+				DELETE respuestas FROM respuestas
+					LEFT JOIN preguntas_respuestas ON preguntas_respuestas.id_respuesta = respuestas.id
+						WHERE preguntas_respuestas.id_pregunta IS NULL;
+				SET @using_delete_trigger = FALSE;
+                
+             END IF;
         END $$
         
-CREATE TRIGGER block_respuestas_delete BEFORE DELETE ON respuestas
+CREATE TRIGGER delete_orphan_respuestas AFTER DELETE ON respuestas
 	FOR EACH ROW
 		BEGIN
-			IF NOT @using_delete_trigger THEN
-				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede borrar una respuesta directamente';
-			END IF;
+			IF NOT @using_delete_trigger THEN	/*Para evitar que el trigger se dispare si es llamada por el otro trigger de borrado*/
+            
+				SET @using_delete_trigger = TRUE;
+				DELETE preguntas FROM preguntas
+					LEFT JOIN preguntas_respuestas ON preguntas_respuestas.id_pregunta = preguntas.id
+						WHERE preguntas_respuestas.id_respuesta IS NULL;
+				SET @using_delete_trigger = FALSE;
+                
+            END IF; 
         END $$
     
-CREATE FUNCTION get_pregunta_id(pregunta VARCHAR(255)) RETURNS INT DETERMINISTIC
+CREATE FUNCTION get_pregunta_id(pregunta VARCHAR(255)) RETURNS INT DETERMINISTIC READS SQL DATA
 	BEGIN
-		DECLARE id_pregunta INT DEFAULT 0;
+		DECLARE id_pregunta INT UNSIGNED DEFAULT 0;
         SELECT COALESCE(id, 0) INTO id_pregunta FROM preguntas WHERE cadena = pregunta;
         RETURN id_pregunta;
     END $$
     
-CREATE FUNCTION get_respuesta_id(respuesta VARCHAR(255)) RETURNS INT DETERMINISTIC
+CREATE FUNCTION get_respuesta_id(respuesta VARCHAR(255)) RETURNS INT DETERMINISTIC READS SQL DATA
 	BEGIN
-		DECLARE id_respuesta INT DEFAULT 0;
+		DECLARE id_respuesta INT UNSIGNED DEFAULT 0;
         SELECT COALESCE(id, 0) INTO id_respuesta FROM respuestas WHERE cadena = respuesta;
         RETURN id_respuesta;
     END $$
     
+CREATE FUNCTION get_pregunta_cadena(id_pregunta INT UNSIGNED) RETURNS VARCHAR(255) DETERMINISTIC READS SQL DATA
+	BEGIN
+		DECLARE pregunta VARCHAR(255) DEFAULT "Pregunta no existente";
+        SELECT COALESCE(cadena, "Pregunta no existente") INTO pregunta FROM preguntas WHERE id = id_pregunta;
+        RETURN pregunta;
+    END $$
+    
+CREATE FUNCTION get_respuesta_cadena(id_respuesta INT UNSIGNED) RETURNS VARCHAR(255) DETERMINISTIC READS SQL DATA
+	BEGIN
+		DECLARE respuesta VARCHAR(255) DEFAULT "Respuesta no existente";
+        SELECT COALESCE(respuesta, "Respuesta no existente") INTO respuesta FROM respuestas WHERE id = id_respuesta;
+        RETURN respuesta;
+    END $$
+    
+CREATE FUNCTION get_respuesta_from_pregunta(pregunta VARCHAR(255)) RETURNS INT UNSIGNED NOT DETERMINISTIC READS SQL DATA
+	BEGIN
+		DECLARE pregunta_id INT UNSIGNED DEFAULT 0;
+        DECLARE respuesta_id INT UNSIGNED DEFAULT 0;
+        DECLARE random_row INT UNSIGNED DEFAULT 0;
+        DECLARE i INT UNSIGNED DEFAULT 1;
+        DECLARE cur1 CURSOR FOR
+					SELECT id FROM respuestas INNER JOIN preguntas_respuestas ON preguntas_respuestas.id_respuesta = respuestas.id
+					WHERE preguntas_respuestas.id_pregunta = pregunta_id;
+                
+		SET pregunta_id = get_pregunta_id(pregunta);
+        IF pregunta_id = 0 THEN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La pregunta no existe';
+        ELSE
+			SELECT CEIL(RAND() * (SELECT COUNT(*) FROM preguntas_respuestas WHERE id_pregunta = pregunta_id)) INTO random_row;
+			OPEN cur1;
+				
+			read_loop: LOOP
+				IF i > random_row THEN
+					LEAVE read_loop;
+				END IF;
+                FETCH cur1 INTO respuesta_id;
+				SET i = i+1;
+			END LOOP;
+            CLOSE cur1;
+        END IF;
+        
+        RETURN respuesta_id;
+        
+    END $$
+    
 CREATE PROCEDURE insert_pregunta_respuesta(IN pregunta VARCHAR(255), IN respuesta VARCHAR(255))
 	BEGIN
-		DECLARE last_pregunta_id INT;
-        DECLARE last_respuesta_id INT;
+		DECLARE target_pregunta_id INT UNSIGNED;
+        DECLARE target_respuesta_id INT UNSIGNED;
+        DECLARE CONTINUE HANDLER FOR 1062
+			BEGIN
+				-- Do nothing
+            END;
         SET @using_insert_procedure = TRUE;
         
-        IF(get_pregunta_id(pregunta) = 0) THEN
+        SET target_pregunta_id = get_pregunta_id(pregunta);
+        SET target_respuesta_id = get_respuesta_id(respuesta);
+        
+        IF(target_pregunta_id = 0) THEN
 			INSERT INTO preguntas (cadena) VALUES (pregunta);
-			SET last_pregunta_id = LAST_INSERT_ID();
-		ELSE
-			SET last_pregunta_id = get_pregunta_id(pregunta);
+			SET target_pregunta_id = LAST_INSERT_ID();
 		END IF;
         
-        IF(get_respuesta_id(respuesta) = 0) THEN
+        IF(target_respuesta_id = 0) THEN
 			INSERT INTO respuestas (cadena) VALUES (respuesta);
-			SET last_respuesta_id = LAST_INSERT_ID();
-		ELSE
-			SET last_respuesta_id = get_respuesta_id(respuesta);
+			SET target_respuesta_id = LAST_INSERT_ID();
 		END IF;
         
-        INSERT INTO preguntas_respuestas (id_pregunta, id_respuesta) VALUES (last_pregunta_id, last_respuesta_id);
-        #Capturar excepción
+        INSERT INTO preguntas_respuestas (id_pregunta, id_respuesta) VALUES (target_pregunta_id, target_respuesta_id);
+        
         SET @using_insert_procedure = FALSE;
     END $$
         
@@ -115,6 +168,8 @@ DELIMITER ;
 
 CALL insert_pregunta_respuesta("¿Mercedes me vas a aprobar?", "No te voy a aprobar");
 CALL insert_pregunta_respuesta("¿Cómo te llamas?", "Manu");
+CALL insert_pregunta_respuesta("¿Cómo te llamas?", "Manuel");
+CALL insert_pregunta_respuesta("¿Cómo te llamas?", "Manolo");
 CALL insert_pregunta_respuesta("¿Cúal es tu nombre?", "Manu");
 CALL insert_pregunta_respuesta("¿Cúal es tu color favorito?", "Azul");
 
@@ -122,5 +177,4 @@ SELECT * FROM preguntas;
 SELECT * FROM respuestas;
 SELECT * FROM preguntas_respuestas;
 
-SET SQL_SAFE_UPDATES=0;
-DELETE FROM preguntas WHERE cadena = "¿Cómo te llamas?";
+/*SET SQL_SAFE_UPDATES=0;*/
